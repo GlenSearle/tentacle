5c7534edc369eed421acbe06c35c66f8fa0c7bf0
Glen Searle <Glen.Searle@gmail.com>
2013-09-20 16:48:42 +0100

    Accelerometer
    Arduino code to read from the GY-521 chip while doing a random dance.

--- Tenticle_dance/Tenticle_dance.ino
+++ Tenticle_dance/Tenticle_dance.ino
@@ -6,23 +6,25 @@
  
 void setup() 
 { 
+  Serial.begin(115200);
   hip.attach(3);
-  knee.attach(5);
-  ankle.attach(6);
+  knee.attach(4);
+  ankle.attach(5);
   
 } 
  
  
 void loop() { 
   switch (random(3)) {
-    case 1:
+    case 0:
     hip.write((random(170)+5));
     break;
-    case 2:
+    case 1:
     knee.write((random(170)+5));
     break;
-    case 3:
+    case 2:
     ankle.write((random(170)+5));
+    break;
 }
-delay(random(1000));
+delay(random(500+random(100)*10));
 } 
